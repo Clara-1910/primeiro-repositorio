7ecb37dcfe84dc573d89d8304c17cf0409fec585
Clara-1910 <moreiramacedomariaclara@gmail.com>
2026-07-07 10:53:38 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,60 @@
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+
+
+public class main {
+    public static void main(String[] args) {
+    
+    EntityManagerFactory factory = Persistence.createEntityManagerFactory("obras");
+    
+    EntityManager em = factory.createEntityManager();
+    
+    Obradearte obra = new Obradearte();
+    
+    
+    
+    obra.setAutor("Adir Sodré");
+    obra.setTitulo("Caminho de São Francisco");
+    obra.setProcedencia("Museu da Caixa D'Agua");
+    obra.setTipo("quadro");
+    obra.setDataObra(parseData("25/11/1988"));
+    obra.setTecnica("Óleo sobre tela");
+    obra.setDimensoes("154cm x 154cm");
+    
+    EntityTransaction tx = em.getTransaction();
+    tx.begin();
+    em.persist(obra);
+    tx.commit();
+    em.close();
+    factory.close();
+    
+}
+
+private static Calendar parseData(String data){
+    try {
+        Date date = new SimpleDateFormat("dd/MM/yyyy").parse(data);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar;
+    } catch (ParseException ex) {
+        throw new IllegalArgumentException(ex);
+    }
+}
+}
